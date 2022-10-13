fa1e05e1ce2a2ec740ff718e8aeb0e059b8b1614
oscar anillo <oscarani14@gmail.com>
2022-10-13 15:39:57 -0500

    add mainComponent to a flexbox view

--- src/components/main-component.js
+++ src/components/main-component.js
@@ -3,17 +3,24 @@ import CreationCommonComponent from "./creation-common-component";
 export default function MainComponent(){
     return (
         <main className="main">
-            <div>
-                <h1 className="main-h1">Our creations</h1>
+            <div className="main-row">
+                <div>
+                    <h1 className="main-h1">Our creations</h1>
+                </div>
+                <div>
+                    <button className="see-all-desktop">See All</button>
+                </div>
             </div>
-            <CreationCommonComponent className="deep-earth" text="Deep earth" />
-            <CreationCommonComponent className="night-arcade" text="Night Arcade" />
-            <CreationCommonComponent className="soccer-team-vr" text="soccer team vr" />
-            <CreationCommonComponent className="the-grid" text="The Grid" />
-            <CreationCommonComponent className="from-up-above" text="from up above vr" />
-            <CreationCommonComponent className="pocket-borealis" text="pocket borealis" />
-            <CreationCommonComponent className="the-curiosity" text="the curiosity" />
-            <CreationCommonComponent className="make-fish" text="Make it fisheye" />
+            <div className="main-row">
+                <CreationCommonComponent className="deep-earth" text="Deep earth" />
+                <CreationCommonComponent className="night-arcade" text="Night Arcade" />
+                <CreationCommonComponent className="soccer-team-vr" text="soccer team vr" />
+                <CreationCommonComponent className="the-grid" text="The Grid" />
+                <CreationCommonComponent className="from-up-above" text="from up above vr" />
+                <CreationCommonComponent className="pocket-borealis" text="pocket borealis" />
+                <CreationCommonComponent className="the-curiosity" text="the curiosity" />
+                <CreationCommonComponent className="make-fish" text="Make it fisheye" />
+            </div>
             <div>
                 <button className="bt-see">See all</button>
             </div>
