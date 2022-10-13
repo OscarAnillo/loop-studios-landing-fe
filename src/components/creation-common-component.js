cefcaf76fd0de9f494cbe2c00711c6c9d893334e
oscar anillo <oscarani14@gmail.com>
2022-10-13 18:47:02 -0500

    add hover effect to footer li

--- src/components/creation-common-component.js
+++ src/components/creation-common-component.js
@@ -1,6 +1,6 @@
 export default function CreationCommonComponent({className, text}){
     return (
-        <div className={`${className}`}>
+        <div className={className}>
             <div className="overlay">
                 <h1>{text}</h1>
             </div>
