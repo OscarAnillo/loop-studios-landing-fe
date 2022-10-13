86a4fa388210fb2ac8058e402e67b0064a9ebbcc
oscar anillo <oscarani14@gmail.com>
2022-10-12 22:18:24 -0500

    add articleComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import HeroComponent from './components/hero-component';
 
 import './App.css';
+import ArticleComponent from './components/article-component';
 
 function App() {
   return (
     <div className="App">
       <HeroComponent />
+      <div className="container">
+        <ArticleComponent />
+
+      </div>
     </div>
   );
 }
